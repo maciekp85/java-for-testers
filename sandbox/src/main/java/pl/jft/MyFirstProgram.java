7b168f4a995ce56659f9829a4a177545fd172b92
Maciej Piotrowski <nishi@wimiip.pl>
2016-12-03 23:51:16 +0100

    Added a few functions

--- sandbox/src/main/java/pl/jft/MyFirstProgram.java
+++ sandbox/src/main/java/pl/jft/MyFirstProgram.java
@@ -3,6 +3,26 @@ package pl.jft;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Maciek");
+
+		double l = 5;
+		System.out.println("Area of square with side " + l + " = " + area(l));
+
+		double a = 4, b = 5;
+		System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }
