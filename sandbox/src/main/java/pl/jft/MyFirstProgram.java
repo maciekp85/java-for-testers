256a756e629a90ffa7e059e5993ed1483a3e91b6
Maciej Piotrowski <nishi@wimiip.pl>
2016-12-04 18:48:42 +0100

    Exercises with functions connected with objects

--- sandbox/src/main/java/pl/jft/MyFirstProgram.java
+++ sandbox/src/main/java/pl/jft/MyFirstProgram.java
@@ -7,22 +7,15 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Maciek");
 
-		double l = 5;
-		System.out.println("Area of square with side " + l + " = " + area(l));
+		Square s = new Square(7);
+		System.out.println("Area of square with side " + s.l + " = " + s.area());
 
-		double a = 4, b = 5;
-		System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(3, 4);
+		System.out.println("Area of rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello " + somebody + "!");
 	}
-
-	public static double area(double len) {
-		return len * len;
-	}
-
-	public static double area(double a, double b) {
-		return a * b;
-	}
+	
 }
---
+++ sandbox/src/main/java/pl/jft/Square.java
@@ -0,0 +1,17 @@
+package pl.jft;
+
+/**
+ * Created by nishi on 2016-12-04.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
