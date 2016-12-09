2d9e2f6ab0d67f84cf331dcb1ff0aa5d55d42f4e
Maciej Piotrowski <nishi@wimiip.pl>
2016-12-09 22:50:51 +0100

    Refactoring: Extracted auxiliary methods

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/ContactCreationTests.java
@@ -16,11 +16,11 @@ public class ContactCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login();
     }
 
-    @Test
-    public void testContactCreation() {
-        wd.get("http://localhost/addressbook/");
+    private void login() {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -28,7 +28,25 @@ public class ContactCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("nowy wpis")).click();
+    }
+
+    @Test
+    public void testContactCreation() {
+        goToContactPage();
+        fillContactForm();
+        submitContactCreation();
+        goToHomePage();
+    }
+
+    private void goToHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactForm() {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
         wd.findElement(By.name("firstname")).sendKeys("Maciej");
@@ -41,8 +59,10 @@ public class ContactCreationTests {
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
         wd.findElement(By.name("address")).sendKeys("testowa street\n12-345");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void goToContactPage() {
+        wd.findElement(By.linkText("nowy wpis")).click();
     }
 
     @AfterMethod
