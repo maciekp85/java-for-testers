95fb7938a7675288a5a6afe784eb91ce4975ca64
Maciej Piotrowski <nishi@wimiip.pl>
2016-12-15 23:09:47 +0100

    3.1 if-then-else construction Solving quadratic equation

---
+++ sandbox/src/test/java/pl/jft/EquationTests.java
@@ -0,0 +1,28 @@
+package pl.jft;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by nishi on 2016-12-15.
+ */
+public class EquationTests {
+
+  @Test
+  public void test0() {
+    Equation e = new Equation(1, 1,1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation(1, 2,1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void test2() {
+    Equation e = new Equation(1, 5,6);
+    Assert.assertEquals(e.rootNumber(), 2);
+  }
+}
