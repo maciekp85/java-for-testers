dc062cfb11658c9c0c19227b710c902f532ef152
Maciej Piotrowski <nishi@wimiip.pl>
2016-12-19 21:15:44 +0100

    3.9 Optimalization of transitions between the pages

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/appmanager/NavigationHelper.java
@@ -14,14 +14,27 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.cssSelector("div[id='content'] > h1"))
+            && wd.findElement(By.cssSelector("div[id='content'] > h1")).getText().equals("Grupy")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("grupy"));
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("strona główna"));
   }
 
   public void goToContactPage() {
+    if (isElementPresent(By.cssSelector("div[id='content'] > h1"))
+            && wd.findElement(By.cssSelector("div[id='content'] > h1")).getText().equals("Edytuj / dodaj wpis do książki adresowej")
+            && isElementPresent(By.name("firstname"))) {
+      return;
+    }
     click(By.linkText("nowy wpis"));
   }
 }
