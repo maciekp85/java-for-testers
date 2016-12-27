c6fd37e068ae20485bbe515b2770e6e42fae467e
Maciej Piotrowski <nishi@wimiip.pl>
2016-12-27 21:55:48 +0100

    4.2 Collections

---
+++ sandbox/src/main/java/pl/jft/Collections.java
@@ -0,0 +1,21 @@
+package pl.jft;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by nishi on 2016-12-27.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = new String[]{"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l: languages) {
+      System.out.println("I want to learn " + l);
+    }
+  }
+}
