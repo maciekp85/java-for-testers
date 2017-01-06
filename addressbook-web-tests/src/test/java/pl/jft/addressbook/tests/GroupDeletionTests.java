444dad7c6712cb5112cd637890435931213f0392
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-06 16:44:40 +0100

    5.2 Improvement of test structure

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package pl.jft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.jft.addressbook.model.GroupData;
 
@@ -8,20 +9,27 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-    @Test
-    public void testGroupDeletion() {
+    @BeforeMethod
+    public void ensurePreconditions() {
         app.getNavigationHelper().goToGroupPage();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+    }
+
+    @Test
+    public void testGroupDeletion() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
+        int index = before.size() - 1;
+        app.getGroupHelper().selectGroup(index);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() - 1);
+        Assert.assertEquals(after.size(), index);
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         Assert.assertEquals(before, after);
     }
+
+
 }
