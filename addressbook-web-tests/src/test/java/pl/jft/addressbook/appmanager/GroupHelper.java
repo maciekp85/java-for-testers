43d18f79e90d1c43e4296263fdbd36ee35d4ef3f
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-07 20:51:32 +0100

    5.8 Hashing

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/appmanager/GroupHelper.java
@@ -79,7 +79,7 @@ public class GroupHelper extends HelperBase {
     return isElementPresent(By.name("selected[]"));
   }
 
-  public int getCountGroup() {
+  public int count() {
     return wd.findElements(By.name("selected[]")).size();
   }
 
--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupModificationTests.java
@@ -7,7 +7,6 @@ import pl.jft.addressbook.model.Groups;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.testng.Assert.assertEquals;
 
 /**
  * Created by nishi on 2016-12-10.
@@ -30,8 +29,8 @@ public class GroupModificationTests extends TestBase {
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
     app.group().modify(group);
+    assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
