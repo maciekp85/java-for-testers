872655c2d176bed290fceb3e5d5b481327c94221
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-15 19:43:38 +0100

    6.6 XML-format Extanded Markup Language

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package pl.jft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.jft.addressbook.model.GroupData;
@@ -9,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -17,15 +19,17 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g} ).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroups")
