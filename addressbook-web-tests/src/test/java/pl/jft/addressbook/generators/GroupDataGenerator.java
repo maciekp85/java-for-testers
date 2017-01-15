8efc341ba2a7748c561d052144bfd4b47cda2550
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-15 23:12:08 +0100

    6.7 JSON - format JavaScript Object Notation

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package pl.jft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import pl.jft.addressbook.model.GroupData;
 
@@ -45,9 +47,19 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if(format.equals("xml")) {
       saveAsXml(groups, new File(file));
+    } else if(format.equals("json")) {
+      saveAsJson(groups, new File(file));
     } else {
       System.out.println("Unrecognized format " + format);
     }
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,7 @@
 package pl.jft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,7 +9,6 @@ import pl.jft.addressbook.model.GroupData;
 import pl.jft.addressbook.model.Groups;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,7 +19,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
     String xml = "";
     String line = reader.readLine();
@@ -32,7 +33,21 @@ public class GroupCreationTests extends TestBase {
     return groups.stream().map((g) -> new Object[] {g} ).collect(Collectors.toList()).iterator();
   }
 
-  @Test(dataProvider = "validGroups")
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {}.getType());  // List<GroupData>.class
+    return groups.stream().map((g) -> new Object[] {g} ).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
