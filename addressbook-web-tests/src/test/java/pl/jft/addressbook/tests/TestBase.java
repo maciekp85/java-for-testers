60be9298b1e6ce284752a4835b06f0a58a9177b8
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-19 00:31:48 +0100

    6.10 Test stand configuration

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/TestBase.java
@@ -10,7 +10,8 @@ import pl.jft.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
