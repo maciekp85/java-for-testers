3b6bf430af9011cf917220db0a3e58a623f6f3ac
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-21 20:11:20 +0100

    6.11 Right logs

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/TestBase.java
@@ -1,14 +1,23 @@
 package pl.jft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.jft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by nishi on 2016-12-10.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app =
           new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -18,9 +27,18 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
+  }
 }
