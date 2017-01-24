042cbdbf3c0618930dd78e4cc9576a08a41d62b7
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-24 21:27:24 +0100

    7.5 Switchable UI tests

--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/GroupModificationTests.java
@@ -33,6 +33,6 @@ public class GroupModificationTests extends TestBase {
     assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
+    verifyGroupListInUI();
   }
-
 }
--- addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/jft/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package pl.jft.addressbook.tests;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +10,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.jft.addressbook.appmanager.ApplicationManager;
+import pl.jft.addressbook.model.GroupData;
+import pl.jft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Created by nishi on 2016-12-10.
@@ -40,5 +48,15 @@ public class TestBase {
   @AfterMethod(alwaysRun = true)
   public void logTestStop(Method m) {
     logger.info("Stop test " + m.getName());
+  }
+
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map( (g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
   }
 }
