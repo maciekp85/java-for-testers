5dc4601e9fa683041d749cbf17461276f003626e
Maciej Piotrowski <nishi@wimiip.pl>
2017-01-29 14:28:52 +0100

    8.2 Added new module for tests for mantis

---
+++ mantis-tests/src/test/java/pl/jft/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package pl.jft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import pl.jft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by nishi on 2016-12-10.
+ */
+public class TestBase {
+
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+}
