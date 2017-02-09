6161c76ac3d9231525ee02e0021442bd23eb257a
Maciej Piotrowski <nishi@wimiip.pl>
2017-02-09 22:35:33 +0100

    10.1 Continuous integration server (broken test to run on jenkins)

--- sandbox/src/test/java/pl/jft/EquationTests.java
+++ sandbox/src/test/java/pl/jft/EquationTests.java
@@ -17,7 +17,7 @@ public class EquationTests {
   @Test
   public void test1() {
     Equation e = new Equation(1, 2,1);
-    Assert.assertEquals(e.rootNumber(), 1);
+    Assert.assertEquals(e.rootNumber(), 2);
   }
 
   @Test
